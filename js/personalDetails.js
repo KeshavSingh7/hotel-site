9601242d0b2334e04f164586bf99135372fed1b7
Keshav <keshavsingh2037@gmail.com>
2020-07-31 23:11:31 +0530

    added validation for name fields

--- js/personalDetails.js
+++ js/personalDetails.js
@@ -1,1 +1,19 @@
+const alphaRegEx = /^[a-zA-Z]+$/;
+function validateFname() {
+    
+    var fn = document.getElementById('fname');
+    var fnv = fn.value.trim();
+    if(fnv.length < 3) {
+        fn.parentElement.className = 'col error';
+        fn.parentElement.querySelector('small').innerText='Name cannot be less than 3 words.'
+    }
 
+    else {
+        if(alphaRegEx.test(fnv))
+            fn.parentElement.className = 'col success';
+        else {
+            fn.parentElement.className = 'col error';
+            fn.parentElement.querySelector('small').innerText='Name cannot contain numbers.'
+        }    
+    }
+}
