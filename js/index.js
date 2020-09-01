4cee1f37bf7c168682ab96d3b11c3511c6b65100
Madhav Narayan <madhavnarayanwebd@gmail.com>
2020-09-01 14:30:21 +0530

    curser added

--- js/index.js
+++ js/index.js
@@ -88,4 +88,68 @@ $(document).ready(function () {
   $("#form").submit(function () {
     return flag;
   });
+
+  function scrollBanner() {
+	  $(document).on('scroll', function(){
+		var scrollPos = $(this).scrollTop();
+		$('.parallax-fade-top').css({
+		  'top' : (scrollPos/2.8)+'px',
+		  'opacity' : 1-(scrollPos/600)
+		});
+	  });    
+	}
+	scrollBanner();	
+    
+	//Page cursors
+
+    document.getElementsByTagName("body")[0].addEventListener("mousemove", function(n) {
+        t.style.left = n.clientX + "px", 
+		t.style.top = n.clientY + "px", 
+		e.style.left = n.clientX + "px", 
+		e.style.top = n.clientY + "px", 
+		i.style.left = n.clientX + "px", 
+		i.style.top = n.clientY + "px"
+    });
+    var t = document.getElementById("cursor"),
+        e = document.getElementById("cursor2"),
+        i = document.getElementById("cursor3");
+    function n(t) {
+        e.classList.add("hover"), i.classList.add("hover")
+    }
+    function s(t) {
+        e.classList.remove("hover"), i.classList.remove("hover")
+    }
+    s();
+    for (var r = document.querySelectorAll(".hover-target"), a = r.length - 1; a >= 0; a--) {
+        o(r[a])
+    }
+    function o(t) {
+        t.addEventListener("mouseover", n), t.addEventListener("mouseout", s)
+    }
+		
+	$(document).ready(function(){"use strict";
+	
+		//Scroll indicator
+		
+		var progressPath = document.querySelector('.progress-wrap path');
+		var pathLength = progressPath.getTotalLength();
+		progressPath.style.transition = progressPath.style.WebkitTransition = 'none';
+		progressPath.style.strokeDasharray = pathLength + ' ' + pathLength;
+		progressPath.style.strokeDashoffset = pathLength;
+		progressPath.getBoundingClientRect();
+		progressPath.style.transition = progressPath.style.WebkitTransition = 'stroke-dashoffset 10ms linear';		
+		var updateProgress = function () {
+			var scroll = $(window).scrollTop();
+			var height = $(document).height() - $(window).height();
+			var progress = pathLength - (scroll * pathLength / height);
+			progressPath.style.strokeDashoffset = progress;
+		}
+		updateProgress();
+		$(window).scroll(updateProgress);
+		
+		
+  });
 });
+
+	
+	
