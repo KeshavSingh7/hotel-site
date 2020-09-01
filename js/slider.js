0fb9df9e34b97afee117ffcb323e874a468b1f94
Madhav Narayan <madhavnarayanwebd@gmail.com>
2020-09-01 06:31:41 +0530

    Cursor added

--- js/slider.js
+++ js/slider.js
@@ -2,3 +2,28 @@
 //usename ->Madhav2108
 //username ->KeshavSingh7
 //just go and check out profiles for more such projects;
+
+
+(function($) { "use strict";
+		
+	//Page cursors
+
+    document.getElementsByTagName("body")[0].addEventListener("mousemove", function(n) {
+        t.style.left = n.clientX + "px", 
+		t.style.top = n.clientY + "px", 
+		e.style.left = n.clientX + "px", 
+		e.style.top = n.clientY + "px", 
+		i.style.left = n.clientX + "px", 
+		i.style.top = n.clientY + "px"
+    });
+    var t = document.getElementById("cursor"),
+        e = document.getElementById("cursor2"),
+        i = document.getElementById("cursor3");
+    function n(t) {
+        e.classList.add("hover"), i.classList.add("hover")
+    }
+    function s(t) {
+        e.classList.remove("hover"), i.classList.remove("hover")
+    }
+    	
+  })(jQuery);
