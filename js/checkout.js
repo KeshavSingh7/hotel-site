50c70cffdf1d2611b5368623f2caf3f442cab3d1
Madhav Narayan <madhavnarayanwebd@gmail.com>
2020-09-03 08:23:28 +0530

    section added

--- js/checkout.js
+++ js/checkout.js
@@ -1,4 +1,20 @@
-function pay1() {
+// // Made by team (madhav,keshav)
+//usename ->Madhav2108
+//username ->KeshavSingh7
+//just go and check out profiles for more such projects;
+
+(function($) { "use strict";
+		
+	
+	var pos = 0;
+			window.setInterval(function(){
+				pos++;
+				document.getElementsByClassName('moving-image')[0].style.backgroundPosition = pos + "px 0px";
+			}, 18
+		);
+
+  })(jQuery);
+  function pay1() {
     document.getElementById('method1').style.display = "block";
     document.getElementById('method2').style.display = "none";
     document.getElementById('method3').style.display = "none";    document.getElementById('card').style.color = "white";
