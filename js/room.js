c62b6132ee0d01b0bacbf103c4acba0db5a272cd
Madhav Narayan <madhavnarayanwebd@gmail.com>
2020-09-04 11:32:29 +0530

    bg image added

--- js/room.js
+++ js/room.js
@@ -38,7 +38,12 @@ if($(window).width() > 800) {
 
     });
 }
-
+var pos = 0;
+			window.setInterval(function(){
+				pos++;
+				document.getElementsByClassName('moving-image')[0].style.backgroundPosition = pos + "px 0px";
+			}, 18
+		);
 
 var swiper = new Swiper('.news-slider', {
     effect: 'coverflow',
