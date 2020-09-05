e7b9c5331da3f13b6f62ca61ea68e76ea55e76df
Madhav Narayan <madhavnarayanwebd@gmail.com>
2020-09-05 21:02:44 +0530

    js complete for playing

--- js/movie.js
+++ js/movie.js
@@ -49,5 +49,25 @@
 		$('body').removeClass('logo-cursor');
     });
     
-
+    $(document).ready(function() {
+				
+		/* Video */
+		
+		$(".container").fitVids();
+						
+		$('.vimeo a,.youtube a').on('click', function (e) {
+			e.preventDefault();
+			var videoLink = $(this).attr('href');
+			var classeV = $(this).parent();
+			var PlaceV = $(this).parent();
+			if ($(this).parent().hasClass('youtube')) {
+				$(this).parent().wrapAll('<div class="video-wrapper">');
+				$(PlaceV).html('<iframe frameborder="0" height="333" src="' + videoLink + '?autoplay=1&showinfo=0" title="YouTube video player" width="547"></iframe>');
+			} else {
+				$(this).parent().wrapAll('<div class="video-wrapper">');
+				$(PlaceV).html('<iframe src="' + videoLink + '?autoplay=1&loop=1&autopause=0&muted=1&color=8c6acc" width="500" height="281" frameborder="0" allow="autoplay"></iframe>');
+			}
+		});	
+					
+	});
 })(jQuery);
