0a49e745494896d23097ab3ce88096303c7c0e76
Madhav Narayan Khullar <madhavnarayanwebd@gmail.com>
2020-09-08 16:13:05 +0530

    new dark button    added

--- bill/src/bill.js
+++ bill/src/bill.js
@@ -2,3 +2,16 @@
 //usename ->Madhav2108
 //username ->KeshavSingh7
 //just go and check out profiles for more such projects;
+
+
+(function($) { "use strict";
+		
+	
+	var pos = 0;
+			window.setInterval(function(){
+				pos++;
+				document.getElementsByClassName('moving-image')[0].style.backgroundPosition = pos + "px 0px";
+			}, 18
+		);
+
+  })(jQuery);
